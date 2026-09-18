5d6538619952153755da09e0f187865e11b892d2
Alomgir Hossain <alomgirhosssain71@gmail.com>
2026-09-18 06:44:34 +0600

    feat: solve problem 03 search insert position

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -61,4 +61,43 @@ const search = function (nums, target) {
 
 
 
+// 03. Search Insert Position
+
+const searchInsert = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+};
+
+
+// Test
+// console.log(
+//     "03. Search Insert Position:",
+//     searchInsert([1, 3, 5, 6], 5)
+// );
+
+
+
+
+
+
+
+
+
+
 
