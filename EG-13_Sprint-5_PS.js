f206d5f4c66713bae167c08de1e928e38a1b92ee
Alomgir Hossain <alomgirhosssain71@gmail.com>
2026-09-18 06:42:47 +0600

    feat: solve problem 02 binary search

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -20,9 +20,45 @@ const removeDuplicates = function (nums) {
 
 
 // Test
-const nums01 = [1, 1, 2];
-const k01 = removeDuplicates(nums01);
+// const nums01 = [1, 1, 2];
+// const k01 = removeDuplicates(nums01);
 
-console.log("01. Remove Duplicates:", k01);
+// console.log("01. Remove Duplicates:", k01);
+
+
+
+
+// 02. Binary Search
+
+const search = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+};
+
+
+// Test
+// console.log(
+//     "02. Binary Search:",
+//     search([-1, 0, 3, 5, 9, 12], 9)
+// );
+
+
+
 
 
