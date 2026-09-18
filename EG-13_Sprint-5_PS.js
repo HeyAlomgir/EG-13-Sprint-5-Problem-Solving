c6e29caa89c812298dea01cb76b45ff18914113e
Alomgir Hossain <alomgirhosssain71@gmail.com>
2026-09-18 06:56:58 +0600

    feat: solve problem 06 product of array except self

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -197,6 +197,44 @@ const invertTree = function (root) {
 
 
 
+// 06. Product of Array Except Self
+
+const productExceptSelf = function (nums) {
+    const result = new Array(nums.length).fill(1);
+
+    let prefix = 1;
+
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    let suffix = 1;
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return result;
+};
+
+
+// Test
+// console.log(
+//     "06. Product of Array Except Self:",
+//     productExceptSelf([1, 2, 3, 4])
+// );
+
+
+
+
+
+
+
+
+
+
 
 
 
